c2cb5197613ba7674148414b8fc7b572853b6dfa
Sebastian Pietras <me@spietras.dev>
2025-11-27 23:19:55 +0100

    Added handling age keys for SOPS in devcontainer

--- .devcontainer/image/hooks/create.sh
+++ .devcontainer/image/hooks/create.sh
@@ -24,3 +24,13 @@ if [[ -s /secrets/.ghtoken && -r /secrets/.ghtoken ]]; then
 		echo "extra-access-tokens = github.com=${token}" >>"${configfile}"
 	fi
 fi
+
+# Use age keys for SOPS if they exist
+if [[ -s /secrets/.agekeys && -r /secrets/.agekeys ]]; then
+	confighome="${XDG_CONFIG_HOME:-${HOME}/.config/}"
+
+	# Copy age keys to SOPS config
+	targetfile="${confighome}/sops/age/keys.txt"
+	mkdir --parents "$(dirname "${targetfile}")"
+	cp --force /secrets/.agekeys "${targetfile}"
+fi
